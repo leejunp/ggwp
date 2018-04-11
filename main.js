96a4c159ed031fc73b3830a6e3d9b698c1857bcc
LeeJun Park <31444831+leejunp@users.noreply.github.com>
2018-04-11 14:16:02 -0400

    circle finished

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+//first popup
+var x=0;
+
 function myOpen() {
   $("#pop").fadeIn(700);
 }
@@ -41,4 +44,37 @@ function mySubmit() {
       $("#popred").fadeOut(700);
     },3000);
   }
+}
+
+//second popup
+
+function myOpenTwo() {
+  $("#poptwo").fadeIn(700);
+}
+
+function mySubmittwo() {
+  if($("#bug").val().length > 0){
+    $("#bug").fadeOut(700);
+    $("#ntwo").fadeOut(700);
+    $("#sutwo").fadeOut(700);
+    $("#cantwo").fadeOut(700);
+    setTimeout(function(){
+      $("#hiddennewstwo").fadeIn(700);
+      $("#closetwo").fadeIn(700);
+    },1000);
+  }
+  else{
+    $("#popredtwo").fadeIn(700);
+    setTimeout(function(){
+      $("#popredtwo").fadeOut(700);
+    },3000);
+  }
+}
+
+function myCanceltwo(){
+  $("#poptwo").fadeOut(700);
+}
+
+function myCloseAlltwo(){
+  $("#poptwo").fadeOut(700);
 }
